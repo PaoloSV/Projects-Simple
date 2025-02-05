a0a84cbb3b2abee65a82cfc4a9f60851ef318e25
PaoloSV <tlv_159753@hotmail.com>
2025-02-05 17:31:17 -0500

    PB-2 calculadora de BMI (finish)

--- 02-BMI/index.js
+++ 02-BMI/index.js
@@ -1,3 +1,16 @@
+
+//captura los valores con el DOM 
+
 const calculateBMI = () => {
-    console.log('Hola');
+    const peso = parseFloat(document.querySelector('#weight').value)
+    const altura = parseFloat(document.querySelector('#height').value)/100
+    const result = document.querySelector('#result');
+    if(peso,altura){
+        const bmi = (peso/(altura*altura)).toFixed(2);
+        result.innerHTML = `Tu BMI:${bmi}`
+    }else {
+        result.style.color = 'red'
+        result.innerHTML= `Porfavor introduce valores Validos`
+    }
+
 }
